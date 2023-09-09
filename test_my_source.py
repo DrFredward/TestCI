d84728ba836a90737c6a5277a57f846c0a5b0006
DrFredward <108410263+DrFredward@users.noreply.github.com>
2023-09-09 18:05:43 +0200

    Add new test

--- test_my_source.py
+++ test_my_source.py
@@ -9,3 +9,7 @@ def test_add_one():
 @pytest.mark.xfail  # markiert pytest, dass ein Fehler erwartet wird
 def test_add_fail():
     assert add_func(5, 5) == 9
+
+
+def test_add_two():
+    assert add_func(4,4) == 7
